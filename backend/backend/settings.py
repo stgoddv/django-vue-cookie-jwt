096add4300a8b9d2ef5247b30f81cd5be3cb2892
santiago <stgoddv@gmail.com>
2020-01-27 03:16:57 -0300

    added example code for auth and unath requests

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -36,6 +36,7 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
     'access-control-allow-origin',
     'access-control-allow-credentials',
 ]
+
 CORS_ORIGIN_WHITELIST = [
     'http://127.0.0.1:8080',
 ]
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -5,8 +5,86 @@
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "home",
-  components: {}
+  components: {},
+  async created() {
+    const options = {
+      baseURL: "http://127.0.0.1:8000",
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "http://127.0.0.1:8000",
+        "Access-Control-Allow-Credentials": true
+      },
+      xsrfCookieName: "csrftoken",
+      xsrfHeaderName: "X-CSRFTOKEN",
+      withCredentials: true,
+      timeout: 5000
+    };
+
+    const instance = axios.create(options);
+
+    console.log("asking for data unauthorized");
+    try {
+      let r = await instance.get("auth/");
+      console.log(r);
+    } catch (error) {
+      if (error.response.status === 401) {
+        console.log("Unauthorized");
+      }
+    }
+
+    console.log("Login in");
+    let payload = {
+      username: "admin",
+      password: "adminadmin"
+    };
+    let r = await instance.post("api/token/", payload);
+    console.log(r);
+
+    console.log("asking for data unauthorized");
+    try {
+      r = await instance.get("auth/");
+      console.log(r);
+    } catch (error) {
+      console.log(error.response);
+    }
+
+    // console.log("Login out");
+    // try {
+    //   r = await instance.post("api/token/delete/");
+    //   console.log(r);
+    // } catch (error) {
+    //   console.log(error.response);
+    // }
+
+    // Request refresh
+    console.log("requesting refresh");
+    try {
+      r = await instance.post("api/token/refresh/");
+      console.log(r);
+    } catch (error) {
+      console.log(error.response);
+    }
+
+    // console.log("Login out");
+    // try {
+    //   r = await instance.post("api/token/delete/");
+    //   console.log(r);
+    // } catch (error) {
+    //   console.log(error.response);
+    // }
+
+    // // Request refresh
+    // console.log("requesting refresh");
+    // try {
+    //   r = await instance.post("api/token/refresh/");
+    //   console.log(r);
+    // } catch (error) {
+    //   console.log(error.response);
+    // }
+  }
 };
 </script>
